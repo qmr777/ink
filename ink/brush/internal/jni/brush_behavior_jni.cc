f0051b2d92fb3e29d3c14d901cbee70b1cf574a6
Samuel Freilich <sfreilich@google.com>
2026-05-01 12:29:05 -0700

    Factor KMP interface out of BrushBehavior JNI
    PiperOrigin-RevId: 908848928

--- ink/brush/internal/jni/brush_behavior_jni.cc
+++ ink/brush/internal/jni/brush_behavior_jni.cc
@@ -14,14 +14,15 @@
 
 #include <jni.h>
 
+#include <cstdint>
 #include <utility>
 #include <variant>
 #include <vector>
 
-#include "absl/functional/overload.h"
 #include "absl/log/absl_check.h"
 #include "absl/status/status.h"
 #include "ink/brush/brush_behavior.h"
+#include "ink/brush/internal/jni/brush_behavior_native.h"
 #include "ink/brush/internal/jni/brush_native_helper.h"
 #include "ink/jni/internal/jni_defines.h"
 #include "ink/jni/internal/jni_string_util.h"
@@ -30,26 +31,8 @@
 namespace {
 
 using ::ink::BrushBehavior;
-using ::ink::brush_internal::ValidateBrushBehaviorTopLevel;
 using ::ink::jni::JStringToStdString;
-using ::ink::jni::ThrowExceptionFromStatus;
-using ::ink::native::CastToBrushBehavior;
-using ::ink::native::CastToBrushBehaviorNode;
-using ::ink::native::DeleteNativeBrushBehavior;
-using ::ink::native::NewNativeBrushBehavior;
-using ::ink::native::NewNativeBrushBehaviorNode;
-
-static constexpr int kSourceNode = 0;
-static constexpr int kConstantNode = 1;
-static constexpr int kNoiseNode = 2;
-static constexpr int kToolTypeFilterNode = 3;
-static constexpr int kDampingNode = 4;
-static constexpr int kResponseNode = 5;
-static constexpr int kIntegralNode = 6;
-static constexpr int kBinaryOpNode = 7;
-static constexpr int kInterpolationNode = 8;
-static constexpr int kTargetNode = 9;
-static constexpr int kPolarTargetNode = 10;
+using ::ink::jni::ThrowExceptionFromStatusCallback;
 
 }  // namespace
 
@@ -58,72 +41,45 @@ extern "C" {
 JNI_METHOD(brush, BrushBehaviorNative, jlong, createFromOrderedNodes)
 (JNIEnv* env, jobject thiz, jlongArray node_native_pointer_array,
  jstring developer_comment) {
-  std::vector<BrushBehavior::Node> nodes;
   const jsize num_nodes = env->GetArrayLength(node_native_pointer_array);
-  nodes.reserve(num_nodes);
   jlong* node_pointers =
       env->GetLongArrayElements(node_native_pointer_array, nullptr);
   ABSL_CHECK(node_pointers != nullptr);
-  for (jsize i = 0; i < num_nodes; ++i) {
-    nodes.push_back(CastToBrushBehaviorNode(node_pointers[i]));
-  }
+  int64_t result = BrushBehaviorNative_createFromOrderedNodes(
+      env, node_pointers, num_nodes,
+      JStringToStdString(env, developer_comment).c_str(),
+      &ThrowExceptionFromStatusCallback);
   env->ReleaseLongArrayElements(
       node_native_pointer_array, node_pointers,
       // No need to copy back the array, which is not modified.
       JNI_ABORT);
-  BrushBehavior behavior{
-      .nodes = std::move(nodes),
-      .developer_comment = JStringToStdString(env, developer_comment),
-  };
-  if (absl::Status status = ValidateBrushBehaviorTopLevel(behavior);
-      !status.ok()) {
-    ThrowExceptionFromStatus(env, status);
-    return 0;
-  }
-  return NewNativeBrushBehavior(std::move(behavior));
+  return result;
 }
 
 JNI_METHOD(brush, BrushBehaviorNative, void, free)
 (JNIEnv* env, jobject thiz, jlong native_pointer) {
-  DeleteNativeBrushBehavior(native_pointer);
+  BrushBehaviorNative_free(native_pointer);
 }
 
 JNI_METHOD(brush, BrushBehaviorNative, jint, getNodeCount)
 (JNIEnv* env, jobject thiz, jlong native_pointer) {
-  return CastToBrushBehavior(native_pointer).nodes.size();
+  return BrushBehaviorNative_getNodeCount(native_pointer);
 }
 
 JNI_METHOD(brush, BrushBehaviorNative, jint, getNodeTypeInt)
 (JNIEnv* env, jobject thiz, jlong native_pointer, jint index) {
-  constexpr auto visitor = absl::Overload{
-      [](const BrushBehavior::SourceNode&) { return kSourceNode; },
-      [](const BrushBehavior::ConstantNode&) { return kConstantNode; },
-      [](const BrushBehavior::NoiseNode&) { return kNoiseNode; },
-      [](const BrushBehavior::ToolTypeFilterNode&) {
-        return kToolTypeFilterNode;
-      },
-      [](const BrushBehavior::DampingNode&) { return kDampingNode; },
-      [](const BrushBehavior::ResponseNode&) { return kResponseNode; },
-      [](const BrushBehavior::IntegralNode&) { return kIntegralNode; },
-      [](const BrushBehavior::BinaryOpNode&) { return kBinaryOpNode; },
-      [](const BrushBehavior::InterpolationNode&) {
-        return kInterpolationNode;
-      },
-      [](const BrushBehavior::TargetNode&) { return kTargetNode; },
-      [](const BrushBehavior::PolarTargetNode&) { return kPolarTargetNode; }};
-  return std::visit(visitor, CastToBrushBehavior(native_pointer).nodes[index]);
+  return BrushBehaviorNative_getNodeTypeInt(native_pointer, index);
 }
 
 JNI_METHOD(brush, BrushBehaviorNative, jstring, getDeveloperComment)
 (JNIEnv* env, jobject object, jlong native_pointer) {
-  const BrushBehavior& brush_behavior = CastToBrushBehavior(native_pointer);
-  return env->NewStringUTF(brush_behavior.developer_comment.c_str());
+  return env->NewStringUTF(
+      BrushBehaviorNative_getDeveloperComment(native_pointer));
 }
 
 JNI_METHOD(brush, BrushBehaviorNative, jlong, newCopyOfNode)
 (JNIEnv* env, jobject thiz, jlong native_pointer, jint index) {
-  return NewNativeBrushBehaviorNode(
-      CastToBrushBehavior(native_pointer).nodes[index]);
+  return BrushBehaviorNative_newCopyOfNode(native_pointer, index);
 }
 
 }  // extern "C"
